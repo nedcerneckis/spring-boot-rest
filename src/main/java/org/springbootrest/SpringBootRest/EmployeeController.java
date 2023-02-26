a48d3fc4a7160a9cf43679b50dd8cbf5375e7f1b
Nedas Cerneckis <nedcerneckis@gmail.com>
2023-02-26 21:34:55 +0000

    added collectionmodel to GET employees

--- src/main/java/org/springbootrest/SpringBootRest/EmployeeController.java
+++ src/main/java/org/springbootrest/SpringBootRest/EmployeeController.java
@@ -1,5 +1,6 @@
 package org.springbootrest.SpringBootRest;
 
+import org.springframework.hateoas.CollectionModel;
 import org.springframework.hateoas.EntityModel;
 import org.springframework.web.bind.annotation.*;
 
@@ -17,12 +18,16 @@ public class EmployeeController {
   }
 
   @GetMapping("/employees")
-  List<Employee> all(){
-    return this.repository.findAll();
+  CollectionModel<EntityModel<Employee>> all(){
+    List<EntityModel<Employee>> employees = repository.findAll().stream().map(
+      employee -> EntityModel.of(employee,
+        linkTo(methodOn(EmployeeController.class).one(employee.getId())).withSelfRel(),
+        linkTo(methodOn(EmployeeController.class).all()).withRel("employees"))).toList();
+    return CollectionModel.of(employees, linkTo(methodOn(EmployeeController.class).all()).withSelfRel());
   }
 
   @GetMapping("employees/{id}")
-  EntityModel one(@PathVariable Long id){
+  EntityModel<Employee> one(@PathVariable Long id){
     Employee employee = repository.findById(id).orElseThrow(
         () -> new EmployeeNotFoundException(id)
     );
